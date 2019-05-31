4f2cc819f98a9e15528be0520dc2f7cfae88585c
RicardoPauloCunha <rpaulo1232@gmail.com>
2019-05-31 11:17:35 -0300

    Arquivos 31/05/2019

--- firebase/senai-svigufo-react-firebase/src/index.js
+++ firebase/senai-svigufo-react-firebase/src/index.js
@@ -5,6 +5,11 @@ import App from './App';
 import EventoIndex from './pages/eventos/index';
 import * as serviceWorker from './serviceWorker';
 
+// import './assents/css/flexbox.css';
+// import './assents/css/reset.css';
+// import './assents/css/style.css';
+
+
 ReactDOM.render(<EventoIndex />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
